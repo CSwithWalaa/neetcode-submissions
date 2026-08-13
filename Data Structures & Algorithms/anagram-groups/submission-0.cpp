71dea45906b03158ae906b6f4c79b30522220715
walaa <dawoudwalaa400@gmail.com>
2026-08-13 21:31:14 +0300

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    // Sorting O(m.n log n)
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+ 
+        unordered_map<string, vector<string>> res;
+        for(const auto& s : strs) {                    // O(m)
+            string sortedS = s;                        // O(n) - copying the string (suppose n its length)
+            sort(sortedS.begin(), sortedS.end());      // O(n log n)
+            res[sortedS].push_back(s);
+        }
+
+        vector<vector<string>> result;
+        for(auto& pair : res) {
+            result.push_back(pair.second);
+        }
+        return result;
+    }
+};
