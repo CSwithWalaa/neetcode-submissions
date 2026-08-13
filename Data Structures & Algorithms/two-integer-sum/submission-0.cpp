3dad9a1db0eef5218e6cbdaf585612cb7019d0a3
walaa <dawoudwalaa400@gmail.com>
2026-08-13 11:23:22 +0300

    Refactor comments and clean up code structure
    Updated comments for clarity and removed alternative approaches.

--- Data Structures & Algorithms/two-integer-sum/submission-0.cpp
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         // Approach 1 : Hash Map (Two Pass)
-        // Time complexity: O(n)
-        // Space complexity: O(n)
+        // Time O(n) _ Space O(n)
 
         // [key _ info] -> [value _ index]
         unordered_map<int, int> indicies;
@@ -12,7 +11,6 @@ public:
         for(int num : nums) {                       // O(n)
             indicies[num] = index++;
         }
-
         
         for(int i = 0; i < nums.size(); i++) {      // O(n)
             int diff = target - nums[i];
@@ -29,11 +27,10 @@ public:
     }
 };
 
-// Alternative Approach
+// Approach 2 (Alternative): Hash Map (One Pass) 
+// Loops the array only once
 /*
-    // Approach 2 : Hash Map (One Pass)
-    // Time complexity: O(n) - Loops the array only once
-    // Space complexity: O(n)
+    // Time O(n) _ Space O(n)
 
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> indicies;
@@ -50,14 +47,15 @@ public:
 
         return {};
     }
+*/
 
-    // Approach 3 : Brute Force
-    // Time complexity: O() 
-    // Space complexity: O(1)
+// Approach 3 (Brute Force)
+/*
+    // Time O(n2) _ Space O(1)
 
     vector<int> twoSum(vector<int>& nums, int target) {
 
-        for(int i = 0; i < nums.size(); i++) {      // O(n)
+        for(int i = 0; i < nums.size(); i++) {              // O(n2)
             for(int j = i + 1; j < nums.size(); j++) {
                 if(nums[i] + nums[j] == target) {
                     return {i, j};
@@ -67,5 +65,4 @@ public:
 
         return {};
     }
-
 */
