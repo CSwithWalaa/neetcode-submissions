f78f886a0e7ff2ad025d4ea487c2b218efa8d310
walaa <dawoudwalaa400@gmail.com>
2026-08-13 21:35:33 +0300

    Clarify time complexity in groupAnagrams function
    Updated time complexity comments for clarity.

--- Data Structures & Algorithms/anagram-groups/submission-0.cpp
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -4,7 +4,7 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
  
         unordered_map<string, vector<string>> res;
-        for(const auto& s : strs) {                    // O(m)
+        for(const auto& s : strs) {                    // O(m.n log n) - m is number of strings 
             string sortedS = s;                        // O(n) - copying the string (suppose n its length)
             sort(sortedS.begin(), sortedS.end());      // O(n log n)
             res[sortedS].push_back(s);
